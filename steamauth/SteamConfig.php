956cf03e6e233a4c33c3f510d6cef6b0a344b96d
fattah Ranjbar <fattah.ranjbar@gmail.com>
2022-09-03 23:32:10 +0430

    Update SteamConfig.php

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,5 +1,6 @@
 <?php
-// Version v1.0
+// Version v1.1
+// Version fanishah.ir
 $steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
@@ -10,4 +11,6 @@ if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; 
 if (empty($steamauth['domainname'])) {$steamauth['domainname'] = $_SERVER['SERVER_NAME'];}
 if (empty($steamauth['logoutpage'])) {$steamauth['logoutpage'] = $_SERVER['PHP_SELF'];}
 if (empty($steamauth['loginpage'])) {$steamauth['loginpage'] = $_SERVER['PHP_SELF'];}
+// Version v1.1
+// Version fanishah.ir
 ?>
